d0dbf299ae261166d6d52c02efb8efde7d68ae9c
ddkeen <d3keen@gmail.com>
2021-03-09 12:22:31 -0500

    Added Person Class Stub

--- Person.java
+++ Person.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+
 /**
  * @description 
  * @author Bradley Grose, Ellie Barry, David Keen, David Morrison
  */
 public class Person {
     
+    private String name;
+    private ArrayList<Crime> crimeList;
+    private boolean livingStatus;
+
+    public Person(String name, ArraList<Crime> crimeList, boolean livingStatus) {
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
