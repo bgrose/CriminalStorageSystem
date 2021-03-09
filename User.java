cef8b776e8f4dad0826ddfebf5166ba95e9cf8ac
David Morrison <77455652+Dave0710@users.noreply.github.com>
2021-03-09 16:02:35 -0500

    Inserted code for User Class

--- User.java
+++ User.java
@@ -3,5 +3,47 @@
  * @author Bradley Grose, Ellie Barry, David Keen, David Morrison
  */
 public class User {
-    
+        
+    private String username;
+    private String password;
+    private String name;
+    private String position;
+    private String admin;
+
+    public boolean isValid()
+    {
+        return true;
+    }
+
+    public boolean isAdmin()
+    {
+        return true;
+    }
+
+    public String getName()
+    {
+        return "";
+    }
+
+    public User(String un, String pw, String nam, String posit, String ad)
+    {
+
+    }
+
+    public User getUser()
+    {
+
+    }
+
+    public void changeUser(User change) 
+    {
+
+    }
+
+    public User()
+    {
+
+    }
+
+
 }
