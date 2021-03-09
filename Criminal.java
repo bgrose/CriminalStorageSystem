5cb0098edfe5d1269c9af67f5da2814524d7adf9
ddkeen <d3keen@gmail.com>
2021-03-09 12:16:15 -0500

    Added Criminal Class Stub

--- Criminal.java
+++ Criminal.java
@@ -3,5 +3,175 @@
  * @author Bradley Grose, Ellie Barry, David Keen, David Morrison
  */
 public class Criminal extends Person{
-    
+
+    private String alias;
+    private String accomplice;
+    private String hairColor;
+    private String eyeColor;
+    private String tatoo;
+    private String skinColor;
+    private String nationality;
+    private double weight;
+    private int height;
+    private String acquiantance;
+    private int age;
+    private boolean glasses;
+    private String punishment;
+    private String disability;
+    private String handness;
+    private String crimeType;
+    private Officer arrestingOfficer;
+
+    public Criminal() {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public String getAlias() {
+        return "";
+    }
+
+    public void setAlias(String alias) {
+
+    }
+
+    public String getAccomplice() {
+        return "";
+    }
+
+    public void setAccomplice(String accomplice) {
+
+    }
+
+    public String getHairColor() {
+        return "";
+    }
+
+    public void setHairColor(String hairColor) {
+
+    }
+
+    public String getEyeColor() {
+        return "";
+    }
+
+    public void setEyeColor(String eyeColor) {
+
+    }
+
+    public String getTattoo() {
+        return "";
+    }
+
+    public void setTatoo(String tatoo) {
+
+    }
+
+    public String getSkinColor() {
+        return "";
+    }
+
+    public void setSkinColor(String skinColor) {
+
+    }
+
+    public String getNationality() {
+        return "";
+    }
+
+    public String setNationality(String nationality) {
+
+    }
+
+    public double getWeight() {
+        return 0.0;
+    }
+
+    public void setWeight(double weight) {
+
+    }
+
+    public int getHeight() {
+        return 0;
+    }
+
+    public void setHeight(int height) {
+
+    }
+
+    public String getAccquaintance() {
+        return "";
+    }
+
+    public void setAccquaintance(String accquaintance) {
+        
+    }
+
+    public int getAge() {
+        return 0;
+    }
+
+    public void setAge(int age) {
+
+    }
+
+    public boolean getGlasses() {
+        return true;
+    }
+
+    public void setGlasses(boolean glasses) {
+
+    }
+
+    public String getPunishment() {
+        return "";
+    }
+
+    public void setPunishment(String punishment) {
+
+    }
+
+    public String getDisability() {
+        return "";
+    }
+
+    public void setDisability(String disability) {
+
+    }
+
+    public String getHandness() {
+        return "";
+    }
+
+    public void setHandness(String handness) {
+
+    }
+
+    public String getCrimeType() {
+        return "";
+    }
+
+    public void setCrimeType(String crimeType) {
+
+    }
+
+    public Officer getArrestingOfficer() {
+        return Officer;
+    }
+
+    public void setArrestingOfficer(Officer officer) {
+
+    }
+
+    public boolean getInJail() {
+        return true;
+    }
+
+    public void setInJail(boolean inJail) {
+
+    }
+
 }
