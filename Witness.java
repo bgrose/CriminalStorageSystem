91d89927755fce5305c95976f4489bdc0b44efa6
ddkeen <d3keen@gmail.com>
2021-03-09 12:30:18 -0500

    Added Witness Class Stub

--- Witness.java
+++ Witness.java
@@ -3,5 +3,23 @@
  * @author Bradley Grose, Ellie Barry, David Keen, David Morrison
  */
 public class Witness extends Person {
+
+    private String statement;
+
+    public Witness() {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public String getStatement() {
+        return "";
+    }
+
+    public void setStatement(String statement) {
+        
+    }
     
 }
