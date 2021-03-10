366582a39288293d83cab7339597ccf9ff36c451
bgrose <47512745+bgrose@users.noreply.github.com>
2021-03-09 23:03:42 -0500

    Adding Constants

--- DataConstants.java
+++ DataConstants.java
@@ -1,4 +1,5 @@
 public class DataConstants {
+    //User
     protected static final String USER_UUID = "UUID";
     protected static final String USER_USERNAME = "Username";
     protected static final String USER_PASSWORD = "Password";
@@ -6,5 +7,17 @@ public class DataConstants {
     protected static final String USER_POSITION = "Position";
     protected static final String USER_ADMIN = "Admin";
 
-    
+    //Crime
+    protected static final String CRIME_UUID = "UUID";
+
+    //Criminal
+    protected static final String CRIMINAL_UUID = "UUID";
+
+    //POI
+    protected static final String POI_UUID = "UUID";
+
+    //Witness
+    protected static final String WITNESS_UUID = "UUID";
+    protected static final String WITNESS_NAME = "Name";
+    protected static final String WITNESS_STATEMENT = "Statement";
 }
