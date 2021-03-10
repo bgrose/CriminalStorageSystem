5404273bd0a2617334afee145efdbdb990c12440
David Morrison <77455652+Dave0710@users.noreply.github.com>
2021-03-09 19:32:16 -0500

    Inserted code for Crime class

--- Crime.java
+++ Crime.java
@@ -4,4 +4,49 @@
  */
 public class Crime {
     
+    private String description;
+    private String name;
+    private Boolean solved;
+    private int date;
+
+    public void Crime(String typeOfString) 
+    {
+
+    }
+
+    public void setDescription(String desc) 
+    {
+
+    }
+
+    public void motifySolved(boolean editSolve) 
+    {
+
+    }
+
+    public void addEvidence(Evidence new) 
+    {
+
+    }
+
+    public void removeEvidence(Evidence delete) 
+    {
+
+    }
+
+    public void addPerson(Person new) 
+    {
+
+    }
+
+    public void removePerson(Person delete) 
+    {
+
+    }
+
+    public String toString() 
+    {
+
+    }
 }
+
