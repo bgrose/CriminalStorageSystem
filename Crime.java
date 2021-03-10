c1b5761f25630dbeb8d599b4214a7c803ec8e4a6
Ellie <ellie455@gmail.com>
2021-03-10 16:40:18 -0500

    Makes it compile

--- Crime.java
+++ Crime.java
@@ -1,52 +1,80 @@
+
 /**
- * @description 
+ * @description Class that creates a Crime
  * @author Bradley Grose, Ellie Barry, David Keen, David Morrison
  */
+
+import java.util.*;
+
 public class Crime {
-    
+
+    private ArrayList<Evidence> evidenceList;
+    private ArrayList<Person> anyonePersonList;
     private String description;
     private String name;
     private Boolean solved;
     private int date;
 
-    public void Crime(String typeOfString) 
-    {
+    /**
+     * Constructor method that sets up the Crime
+     * @param str
+     */
+    public void Crime(String str) {
 
     }
 
-    public void setDescription(String desc) 
-    {
+    /**
+     * Method that sets the description of the Crime
+     * @param description
+     */
+    public void setDescription(String description) {
 
     }
 
-    public void motifySolved(boolean editSolve) 
-    {
+    /**
+     * Method that mofidy's if the Crime has been solved
+     * @param solved Boolean that states if the Crime has been solved
+     */
+    public void motifySolved(boolean solved) {
 
     }
 
-    public void addEvidence(Evidence newEv) 
-    {
+    /**
+     * Method that adds Evidence to the Crime
+     * @param evidence Evidence to be added from the crime
+     */
+    public void addEvidence(Evidence evidence) {
 
     }
 
-    public void removeEvidence(Evidence delete) 
-    {
+    /**
+     * Method that removes Evidence from the Crime
+     * @param evidence Evidence to be removed from the crime
+     */
+    public void removeEvidence(Evidence evidence) {
 
     }
 
-    public void addPerson(Person newPerson) 
-    {
+    /**
+     * Method that adds a Person to the Crime
+     * @param person Person that is added to the Crime
+     */
+    public void addPerson(Person person) {
 
     }
 
-    public void removePerson(Person delete) 
-    {
+    /**
+     * Method that removes a Person from the Crime
+     * @param person Person that is removed from the Crime
+     */
+    public void removePerson(Person person) {
 
     }
 
-    public String toString() 
-    {
+    /**
+     * Method that prints out the details of the Crime class
+     */
+    public String toString() {
         return "";
     }
 }
-
