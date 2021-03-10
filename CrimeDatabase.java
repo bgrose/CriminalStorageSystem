78ed2a10f04e7a19d50e66d4d74e785994d77e94
Ellie <ellie455@gmail.com>
2021-03-10 16:24:06 -0500

    Made it compile

--- CrimeDatabase.java
+++ CrimeDatabase.java
@@ -1,7 +1,53 @@
+
 /**
  * @description 
  * @author Bradley Grose, Ellie Barry, David Keen, David Morrison
  */
+import java.util.*;
+
 public class CrimeDatabase {
-    
+
+    ArrayList<Crime> crimeDatabase;
+
+    public void CrimeDatabase(ArrayList<Crime> crimeList) {
+
+    }
+
+    /**
+     * Method that
+     * @return
+     */
+    public Crime getCrime() {
+        Crime crime = new Crime();
+        return crime;
+    }
+
+    /**
+     * Method that
+     */
+    public void addCrime(Crime crime) {
+
+    }
+
+    /**
+     * Method that
+     */
+    public void removeCrime(Crime crime) {
+
+    }
+
+    /**
+     * Method that
+     */
+    public void modifyCrime(Crime crime) {
+
+    }
+
+    /**
+     * Method that
+     */
+    public void printDatabase() {
+
+    }
+
 }
