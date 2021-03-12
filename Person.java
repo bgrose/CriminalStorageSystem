82cd34d53daedd346cdb090b7a52d3bbf9fd9e6f
ddkeen <d3keen@gmail.com>
2021-03-12 11:33:24 -0500

    Implemented Person Methods

--- Person.java
+++ Person.java
@@ -11,10 +11,32 @@ public class Person {
     private boolean livingStatus;
 
     public Person(String name, ArrayList<Crime> crimeList, boolean livingStatus) {
-        
+        this.name = name;
+        this.crimeList = crimeList;
+        this.livingStatus = livingStatus;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Crime> getCrimeList() {
+        return this.crimeList;
+    }
+
+    public void setCrimeList(ArrayList<Crime> crimeList) {
+        this.crimeList = crimeList;
+    }
+
+    public boolean getLivingStatus() {
+        return this.livingStatus;
+    }
+
+    public void setLivingStatus(boolean livingStatus) {
+        this.livingStatus = livingStatus;
     }
 }
