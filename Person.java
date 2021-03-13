e6a9046c50d34e5dafd15f00627061b06d8fd900
David Morrison <77455652+Dave0710@users.noreply.github.com>
2021-03-13 15:06:00 -0500

    new JavaDoc

--- Person.java
+++ Person.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 /**
- * @description 
+ * @description Class that creates Person
  * @author Bradley Grose, Ellie Barry, David Keen, David Morrison
  */
 public class Person {
@@ -11,6 +11,10 @@ public class Person {
     private boolean livingStatus;
     private String UUID;
 
+    /**
+     * Constructor method that sets up the Person
+     * @param
+     */
     public Person(String name, ArrayList<Crime> crimeList, boolean livingStatus, String _UUID) {
         this.name = name;
         this.crimeList = crimeList;
@@ -18,10 +22,18 @@ public class Person {
         this.UUID = _UUID;
     }
 
+    /**
+     * Method that sets the name
+     * @param name String name of the Person
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Method that gets the name
+     * @return String name of the Person
+     */
     public void setName(String name) {
         this.name = name;
     }
@@ -30,32 +42,59 @@ public class Person {
         return this.crimeList;
     }
 
+    /**
+     * Method that sets the list of the Crime
+     * @param evidenceList ArrayList<Crime>  of the Crime
+     */
     public void setCrimeList(ArrayList<Crime> crimeList) {
         this.crimeList = crimeList;
     }
 
+    /**
+     * Method that gets the living status
+     * @return livingStatus boolean livingStatus
+     */
     public boolean getLivingStatus() {
         return this.livingStatus;
     }
 
+    /**
+     * Method that sets the living status
+     * @param livingStatus boolean livingStatus of the person
+     */
     public void setLivingStatus(boolean livingStatus) {
         this.livingStatus = livingStatus;
     }
 
+    /**
+     * Method that gets the person's UUID
+     * @return person's UUID
+     */
     public String getUUID(){
-        return this.UUID;
+        return "";
     }
 
+    /**
+     * Method that gets who's the witness
+     * @return true if there is a witness
+     */
     public boolean isWitness() {
         return true;
     }
 
+        /**
+     * Method that gets who's the POI
+     * @return true if there is a POI
+     */
     public boolean isPOI() {
         return true;
     }
 
+    /**
+     * Method that gets who's the criminal
+     * @return true if there is a criminal
+     */
     public boolean isCriminal() {
         return true;
     }
-
 }
