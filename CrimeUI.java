9b10086baf78b3f376dc75c45c0a0b5bde7aa1cd
David Morrison <77455652+Dave0710@users.noreply.github.com>
2021-03-15 17:35:44 -0400

    Inserted JavaDoc

--- CrimeUI.java
+++ CrimeUI.java
@@ -1,19 +1,33 @@
+import java.util.Scanner;
+
 /**
- * @description 
+ * @description creates the main driver of CrimeUI
  * @author Bradley Grose, Ellie Barry, David Keen, David Morrison
  */
-public class CrimeUI {
+public class CrimeUI 
+{
+    private Scanner scanner;
+    private Crime crime;
 
+    /* 
+    * Method that will run the program
+    */
     public void run() 
     {
 
     }
 
+    /* 
+    * Method that will open the Main menu
+    */
     public void displayMenu() 
     {
 
     }
 
+    /* 
+    * Method that will open the Admin menu
+    */
     public void displayAdmin() 
     {
 
