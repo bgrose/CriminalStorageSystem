5611baa0647b81308d64b5ecf388259d4b7d1543
bgrose <47512745+bgrose@users.noreply.github.com>
2021-03-21 13:15:53 -0400

    try Fix

--- CrimeDatabase.java
+++ CrimeDatabase.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class CrimeDatabase {
 
     private static CrimeDatabase crimeDatabase;
-    ArrayList<Crime> crimeList;
+    private static ArrayList<Crime> crimeList;
 
     /**
      * Method that returns a Crime from crimeDatabase
