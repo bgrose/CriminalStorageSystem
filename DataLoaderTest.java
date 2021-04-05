062716ad191dc37e88c01570877be77de57603ff
bgrose <47512745+bgrose@users.noreply.github.com>
2021-04-05 16:55:12 -0400

    DataWriter and Loader tessts

--- DataLoaderTest.java
+++ DataLoaderTest.java
@@ -8,37 +8,50 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DataLoaderTest {
+    private UserDatabase users = UserDatabase.getInstance();
+    private ArrayList<User> userList = users.getDatabase();
+    private CrimeDatabase crimes = CrimeDatabase.getInstance();
+    private ArrayList<Crime> crimeList = crimes.getDatabase();
 
     @BeforeEach
     public void setup() {
-        DataWriter.DataUpdate();
+        UserDatabase.getInstance().getDatabase().clear();
+        CrimeDatabase.getInstance().getDatabase().clear();
+        PersonDatabase.getInstance().getDatabase().clear();
+        DataWriter.saveUsers();
     }
 
     @AfterEach
     public void tearDown() {
-        DataWriter.DataUpdate();
+        UserDatabase.getInstance().getDatabase().clear();
+        CrimeDatabase.getInstance().getDatabase().clear();
+        PersonDatabase.getInstance().getDatabase().clear();
+        DataWriter.saveUsers();
     }
 
     /**
-     * Tested by: Bradley grose
+     * Tested by: Bradley grose 
      * Passed: True
      */
     @Test
     void testGetCrimes() {
-        ArrayList<Crime> crimes = new ArrayList<Crime>();
-        crimes = DataLoader.getCrimes();
-        assertEquals(crimes.size(), 0);
+        crimeList.add(new Crime(null, null, null, "description", "caseID", false, "date"));
+        DataWriter.saveCrimes();
+        ArrayList<Crime> crimesRet = DataLoader.getCrimes();
+        assertEquals(crimesRet.size(), 1);
     }
 
     /**
-     * Tested by: Bradley grose
+     * Tested by: Bradley grose 
      * Passed: True
      */
     @Test
     void testGetUsers() {
-        ArrayList<User> ussers = new ArrayList<User>();
-        ussers = DataLoader.getUsers();
-        assertEquals(ussers.size(), 0);
+
+        userList.add(new User("test", "password", "name", "position", false));
+        DataWriter.saveUsers();
+        ArrayList<User> userRet = DataLoader.getUsers();
+        assertEquals(userRet.size(), 1);
     }
 
     /**
@@ -48,29 +61,40 @@ public class DataLoaderTest {
     @Test
     void testGetPerson() {
         ArrayList<Suspects> suspects = new ArrayList<Suspects>();
-        suspects = DataLoader.getPerson();
-        assertEquals(suspects.size(), 0);
+        suspects.add(new Suspects("name", true, "alias", "accomplice", "hairColor", "eyeColor", "tatoo", "skinColor",
+                "nationality", 100, 100, "acquaintance", 1, true, "punishment", "disability", "handness", "crimeType",
+                true, "Gender", "address", "phone"));
+        crimeList.add(new Crime(null, suspects, null, "description", "caseID", false, "date"));
+        DataWriter.saveCrimes();
+        ArrayList<Suspects> sussRet = DataLoader.getPerson();
+        assertEquals(sussRet.size(), 1);
     }
 
     /**
-     * Tested by: Bradley Grosse
+     * Tested by: Bradley Grosse 
      * Passed: True
      */
     @Test
     void testGetEvidence() {
         ArrayList<Evidence> evidence = new ArrayList<Evidence>();
-        evidence = DataLoader.getEvidence();
-        assertEquals(evidence.size(), 0);
+        evidence.add(new Evidence("name", "description"));
+        crimeList.add(new Crime(evidence, null, null, "description", "caseID", false, "date"));
+        DataWriter.saveCrimes();
+        ArrayList<Evidence> evidenceRet = DataLoader.getEvidence();
+        assertEquals(evidenceRet.size(), 1);
     }
 
     /**
-     * Tested by: Bradley Grose
+     * Tested by: Bradley Grose 
      * Passed: True
      */
     @Test
     void testGetWitness() {
-        ArrayList<Witness> witness = new ArrayList<Witness>();
-        witness = DataLoader.getWitness();
-        assertEquals(witness.size(), 0);
+        ArrayList<Witness> witnesses = new ArrayList<Witness>();
+        witnesses.add(new Witness("name", true, "statement", "Gender", false, "address", "phone"));
+        crimeList.add(new Crime(null, null, witnesses, "description", "caseID", false, "date"));
+        DataWriter.saveCrimes();
+        ArrayList<Witness> witnessRet = DataLoader.getWitness();
+        assertEquals(witnessRet.size(), 1);
     }
 }
--- DataWriterTest.java
+++ DataWriterTest.java
@@ -3,6 +3,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
+import org.json.simple.JSONObject;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +16,6 @@ public class DataWriterTest {
     private ArrayList<Crime> crimeList = crimes.getDatabase();
     private PersonDatabase persons = PersonDatabase.getInstance();
     private ArrayList<Suspects> personList = persons.getDatabase();
-
 
     @BeforeEach
     public void setup() {
@@ -33,18 +34,18 @@ public class DataWriterTest {
     }
 
     /**
-     * Tested by: Bradley Grose
+     * Tested by: Bradley Grose 
      * Passed: True
      */
     @Test
     void testDataUpdate() {
-            DataWriter.DataUpdate();
-            //Cant be Tested
-            assertEquals(true, true);
+        DataWriter.DataUpdate();
+        // Cant be Tested
+        assertEquals(true, true);
     }
 
     /**
-     * Tested by: Bradley Grose
+     * Tested by: Bradley Grose 
      * Passed: True
      */
     @Test
@@ -55,15 +56,17 @@ public class DataWriterTest {
     }
 
     /**
-     * Tested by: Passed:
+     * Tested by: Bradley Grose 
+     * Passed: True
      */
     @Test
     void testGetUserJSON() {
-
+        JSONObject user = DataWriter.getUserJSON((new User("test", "password", "name", "position", false)));
+        assertEquals("test", user.get(DataConstants.USER_USERNAME));
     }
 
     /**
-     * Tested by: Bradley Grose
+     * Tested by: Bradley Grose 
      * Passed: True
      */
     @Test
@@ -74,15 +77,18 @@ public class DataWriterTest {
     }
 
     /**
-     * Tested by: Passed:
+     * Tested by: Bradley Grose 
+     * Passed: True
      */
     @Test
     void testGetCrimeJson() {
-
+        JSONObject crime = DataWriter.getCrimeJSON(new Crime(null, null, null, "description", "caseID", false, "date"),
+                null, null);
+        assertEquals("description", crime.get(DataConstants.CRIME_DESCRIPTION));
     }
 
     /**
-     * Tested by: Bradley Grose
+     * Tested by: Bradley Grose 
      * Passed: True
      */
     @Test
@@ -90,27 +96,27 @@ public class DataWriterTest {
         ArrayList<Evidence> evidence = new ArrayList<Evidence>();
         evidence.add(new Evidence("name", "description"));
         crimeList.add(new Crime(evidence, null, null, "description", "caseID", false, "date"));
-        DataWriter.saveCrimes(); //Calls Evidence
+        DataWriter.saveCrimes(); // Calls Evidence
         assertEquals("name", DataLoader.getCrimes().get(0).getEvidenceList().get(0).getName());
     }
 
     /**
-     * Tested by: Bradley Grose
-     * Passed: Failed
+     * Tested by: Bradley Grose 
+     * Passed: True
      */
     @Test
     void testSaveSuspect() {
         ArrayList<Suspects> suspects = new ArrayList<Suspects>();
-        suspects.add(new Suspects("name", true, "alias", "accomplice", "hairColor", "eyeColor", "tatoo", "skinColor", 
-                        "nationality", 100, 100, "acquaintance", 1, true, "punishment", "disability", "handness", "crimeType", 
-                        true, "Gender", "address", "phone"));
+        suspects.add(new Suspects("name", true, "alias", "accomplice", "hairColor", "eyeColor", "tatoo", "skinColor",
+                "nationality", 100, 100, "acquaintance", 1, true, "punishment", "disability", "handness", "crimeType",
+                true, "Gender", "address", "phone"));
         crimeList.add(new Crime(null, suspects, null, "description", "caseID", false, "date"));
-        DataWriter.saveCrimes(); //Calls Evidence
+        DataWriter.saveCrimes(); // Calls Evidence
         assertEquals("name", DataLoader.getCrimes().get(0).getPersonList().get(0).getName());
     }
 
     /**
-     * Tested by: Bradley Grose
+     * Tested by: Bradley Grose 
      * Passed: True
      */
     @Test
@@ -118,30 +124,34 @@ public class DataWriterTest {
         ArrayList<Witness> witnesses = new ArrayList<Witness>();
         witnesses.add(new Witness("name", true, "statement", "Gender", false, "address", "phone"));
         crimeList.add(new Crime(null, null, witnesses, "description", "caseID", false, "date"));
-        DataWriter.saveCrimes(); //Calls Evidence
+        DataWriter.saveCrimes(); // Calls Evidence
         assertEquals("name", DataLoader.getCrimes().get(0).getWitnessList().get(0).getName());
     }
 
     /**
-     * Tested by: Bradley Grose
+     * Tested by: Bradley Grose 
      * Passed: True
      */
     @Test
     void testSavePersons() {
-        personList.add(new Suspects("name", true, "alias", "accomplice", "hairColor", "eyeColor", "tatoo", "skinColor", 
-                                                        "nationality", 100, 100, "acquaintance", 1, true, "punishment", "disability", "handness", "crimeType", 
-                                                        true, "Gender", "address", "phone"));
+        personList.add(new Suspects("name", true, "alias", "accomplice", "hairColor", "eyeColor", "tatoo", "skinColor",
+                "nationality", 100, 100, "acquaintance", 1, true, "punishment", "disability", "handness", "crimeType",
+                true, "Gender", "address", "phone"));
         DataWriter.savePersons();
         assertEquals("name", DataLoader.getPerson().get(0).getName());
-        
+
     }
 
     /**
-     * Tested by: Passed:
+     * Tested by: Bradley Grose 
+     * Passed: True
      */
     @Test
     void testGetSuspectJSON() {
-
+        JSONObject suspect = DataWriter.getSuspectJSON(new Suspects("name", true, "alias", "accomplice", "hairColor",
+                "eyeColor", "tatoo", "skinColor", "nationality", 100, 100, "acquaintance", 1, true, "punishment",
+                "disability", "handness", "crimeType", true, "Gender", "address", "phone"));
+        assertEquals("name", suspect.get(DataConstants.CRIMINAL_NAME));
     }
 
 }
