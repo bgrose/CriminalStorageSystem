a5ac0d471676e24c90b3bdc3861f4b6b4f489b7f
David Morrison <77455652+Dave0710@users.noreply.github.com>
2021-04-08 23:19:28 -0400

    new inputs for testSuspect and testToString

--- SuspectTest.java
+++ SuspectTest.java
@@ -8,6 +8,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SuspectTest {
+	
+	private String name;
+        private String livingStatus;
+        private String alias;
+        private String accomplice;
+        private String hairColor;
+        private String eyeColor;
+        private String tatoo;
+        private String skinColor;
+        private String nationality;
+        private double weight;
+        private int height;
+        private String acquaintance;
+        private int age;
+        private boolean glasses;
+        private String punishment;
+        private String disability;
+        private String handness;
+        private String crimeType;
+        private boolean inJail;
+        private String Gender;
+        private String address;
+        private int phone;
+	
 	@BeforeEach
 	public void setup() {
 
@@ -23,7 +47,10 @@ public class SuspectTest {
 	 */
 	@Test
 	void testSuspect() {
-
+	    Suspects suspects = new Suspects("name", true, "alias", "accomplice", "hairColor", "eyeColor", "tatoo", "skinColor",
+            "nationality", 100, 100, "acquaintance", 1, true, "punishment", "disability", "handness", "crimeType",
+            true, "Gender", "address", "phone");
+            assertEquals("test", suspects.getName());
 	}
 
 	/**
@@ -31,6 +58,10 @@ public class SuspectTest {
 	 */
 	@Test
 	void testToString() {
-
+            Suspects suspects = new Suspects("name", true, "alias", "accomplice", "hairColor", "eyeColor", "tatoo", "skinColor",
+            "nationality", 100, 100, "acquaintance", 1, true, "punishment", "disability", "handness", "crimeType",
+            true, "Gender", "address", "phone");        
+            String ret = suspects.toString();
+            assertTrue(ret.contains("Statement:"));
 	}
 }
